3777b6708038d428addc4b1d390c3f290a0d09db
Jun <elexjun@gmail.com>
2013-03-05 18:01:51 +0800

    add editarea to assets path

--- lib/editarea-rails.rb
+++ lib/editarea-rails.rb
@@ -3,6 +3,9 @@ require "editarea-rails/version"
 module Editarea
   module Rails
     class Engine < ::Rails::Engine
+      initializer 'editarea.load_static_assets' do |app|
+        app.middleware.use ::ActionDispatch::Static, "#{root}/vendor"
+      end
     end
   end
 end
